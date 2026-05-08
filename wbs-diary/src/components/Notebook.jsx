277b9d9bee9fbf8e467c67605f3ae209c7f83799
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2026-05-08 18:50:07 +0100

    Fixed a small issue

--- wbs-diary/src/components/Notebook.jsx
+++ wbs-diary/src/components/Notebook.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Notebook = () => {
-  const { state } = useLocation();
-  const { username } = state || { username: ''};
   const navigate = useNavigate();
+  const username = localStorage.getItem('currentUser') || '';
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4 pt-20">
